0fa89f70152748ffbfa523d2f127699d9a79d6c9
Mike Fuller <16685216+themikefuller@users.noreply.github.com>
2020-05-11 21:58:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const Menu = () => {
+  let x = document.getElementById("navMenu");
+  if (x.className === "navMenu") {
+    x.className += " responsive";
+  } else {
+    x.className = "navMenu";
+  }
+};
+
 console.log('ready');
