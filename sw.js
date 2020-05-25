ef0e2106141ea440d62e632187df4cf7bdcbec72
Mike Fuller <emailthemike@gmail.com>
2020-05-25 15:51:00 -0400

    updated May 25, 2020 @ 3:50pm

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 'use strict';
 
-/////////////////// v0.0.0+
+//////////////////// v0.0.0+
 
-importScripts('https://cdn.jsdelivr.net/npm/@starbase/starbase@0.4.14');
+importScripts('https://cdn.jsdelivr.net/npm/@starbase/starbase@latest');
 
 const starbase = Starbase();
 
 const cacheName = 'app';
 const resources = [
   '/', '/404.html', '/offline', '/manifest.json', '/styles.css', '/app.js',
-  'https://cdn.jsdelivr.net/npm/@starbase/starbase@0.4.14',
+  'https://cdn.jsdelivr.net/npm/@starbase/starbase@latest',
   '/images/icon-96.png', '/images/icon-120.png', '/images/icon-192.png', '/images/icon-384.png', '/images/icon-512.png',
 ];
 
